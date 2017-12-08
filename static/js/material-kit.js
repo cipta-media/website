8856a8a7585372b29d23e86c03fc87e1051e7653
Dwi Satria Utama <dwi.satria@wikimedia.or.id>
2017-12-08 11:45:28 +0700

    Add redirect

--- static/js/material-kit.js
+++ static/js/material-kit.js
@@ -156,3 +156,13 @@ $(document).ready(function () {
         $('#wrapper').toggleClass('toggled');
   });  
 });
+
+var url = document.location.toString();
+if (url.match('#')) {
+    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+} 
+
+// Change hash for page-reload
+$('.nav-tabs a').on('shown.bs.tab', function (e) {
+    window.location.hash = e.target.hash;
+})
